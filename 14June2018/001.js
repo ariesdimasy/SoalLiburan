cbdb7a6fc3a1d5ab09da4be314995cffd5348865
kunglaw <alhusna901@gmail.com>
2018-06-14 22:54:39 +0700

    add 14June2018

---
+++ 14June2018/001.js
@@ -0,0 +1,24 @@
+function soal1(objList,id,action)
+{
+    // your code here 
+}
+
+console.log(soal1({
+    0:{name:"dimas",codename:"surgeon of death"},
+    1:{name:"awtian",codename:"the black assasin"},
+    2:{name:"icha",codename:"the real dementor"}
+},1,"view"));
+// { name:"awtian",codename:"the black assasin"}
+
+console.log(soal1({
+    0:{name:"dimas",codename:"surgeon of death"},
+    1:{name:"awtian",codename:"the black assasin"},
+    2:{name:"icha",codename:"the real dementor"}
+},1,"delete"));
+/* 
+    { 
+        0:{ name:"dimas",codename:"surgeon of death"},
+        1:{ name:"icha",codename:"the real dementor"}
+    }
+
+*/
---
+++ 14June2018/002.js
@@ -0,0 +1,27 @@
+function soal2(param)
+{
+    // your code here
+}
+
+console.log(soal2(5))
+/*
+
+  [
+      ['','','*','',''],
+      ['','*','*','*',''],
+      ['*','*','*','*','*']
+  ]
+*/
+
+console.log(soal2(3))
+/*
+  [
+      ['','*',''],
+      ['*','*','*']
+  ]
+*/
+
+console.log(soal2(4)) // invalid input
+console.log(soal2(2)) // invalid input
+console.log(soal2(6)) // invalid input
+
