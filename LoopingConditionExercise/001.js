1f33504e8f33a690d84e1e808892060f36a4ad14
Kunglaw <alhusna901@gmail.com>
2018-12-23 11:44:31 +0700

    add Looping Conditional Exercises

---
+++ LoopingConditionExercise/001.js
@@ -0,0 +1,31 @@
+/*
+    ============
+    Generate Bar 
+    ============
+
+    Buatlah Program untuk mengenerate Bar ( persegi Panjang ) berdasarkan 
+    - p => panjang 
+    - t => tinggi 
+
+*/
+
+function generateBar(p,t) {
+    // your code here
+}
+
+generateBar(4,3)//
+/*
+    oooo
+    o  o
+    oooo
+*/
+
+generateBar(10,4) // 
+/*
+    oooooooooo
+    o        o
+    o        o
+    oooooooooo
+*/
+
+generateBar(4,5) // panjang harus lebih besar daripada tinggi 
---
+++ LoopingConditionExercise/002.js
@@ -0,0 +1,52 @@
+/*
+    ===========
+    Bar X dan O
+    ===========
+
+    program ini berfungsi mengenerate Persegi Panjang dengan Character O dan X
+
+    [INSTRUCTION]
+    diberikan 2 buah parameter untuk mengenerate Bar 
+    - p => panjang
+    - t => tinggi 
+
+    character yang pertama kali di cetak adalah x , 
+    lalu o dan setelah itu o kemudian kembali lagi ke x 
+
+    [EXAMPLE]
+
+    generateBarXO(4,3)
+    output: 
+    xoox
+    ooxo
+    oxoo
+    xoox
+
+    generateBarXO(6,4)
+    output: 
+    xooxoo
+    xooxoo
+    xooxoo
+    xooxoo
+
+*/
+
+function generateBarXO(p,t) {
+    // your code here 
+}
+
+generateBarXO(4,3)
+/* 
+xoox
+ooxo
+oxoo
+xoox
+*/
+
+generateBarXO(6,4)
+/*
+xooxoo
+xooxoo
+xooxoo
+xooxoo
+*/
