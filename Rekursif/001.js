2cee071403f83056cc2b7760c17a7fb7d835d782
kunglaw <alhusna901@gmail.com>
2018-12-25 20:12:54 +0700

    add recursive exercise

---
+++ Rekursif/001.js
@@ -0,0 +1,21 @@
+/* 
+    ================
+    addAll recursive
+    ================
+
+    [INSTRUCTION]
+    jumlahkan semua angka yang ada di array dengan menggunakan metode rekursive
+
+
+    [EXAMPLE]
+
+    addAllRecursive([1,2,3,4,5,6]) 
+    output : 21
+
+*/
+
+function addAllRecursive(arr) {
+    // your code here 
+}
+
+addAllRecursive([1,2,3,4,5,6]) // 21
