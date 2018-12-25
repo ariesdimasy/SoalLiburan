1af664e798fdddd697c62266900ef229c68e3317
kunglaw <alhusna901@gmail.com>
2018-12-25 20:21:22 +0700

    add palindromeRecursive

---
+++ Rekursif/002.js
@@ -0,0 +1,31 @@
+/*
+   ====================
+   Palindrome Recursive
+   ====================
+
+   [INSTRUCTION]
+   buatlah sebuah program untuk mendeteksi sebuah kata palindrome atau bukan 
+   dengan menggunakan methode recursive
+   - jika sebuah kata atau kalimat palindrome maka outputnya adalah : true
+   - jika sebuah kata atau kalimat bukan palindrome maka outputnya adalah : false 
+
+   [EXAMPLE]
+   palindromeRecursive("kasur ini rusak") 
+   output: true
+
+   palindromeRecursive("nababan")
+   output: true
+   
+   palindromeRecursive("makan")
+   output: false
+*/
+
+function palindromeRecursive(str) {
+    // your code here 
+}
+
+palindromeRecursive("kasur ini rusak") // true 
+
+palindromeRecursive("nababan") // true
+
+palindromeRecursive("makan") // false
