d993c4323a76be4862488173ca3396e82cadb8fd
kunglaw <alhusna901@gmail.com>
2018-12-26 20:00:35 +0700

    array exercises

---
+++ Array/001.js
@@ -0,0 +1,32 @@
+/*
+    ==================
+    WordToArrayReverse
+    ==================
+
+    [INSTRUCTION]
+    buatlah sebuah program yang mengconvert string menjadi array, yang di reverse
+    posisinya berdasarkan per kata 
+
+    [RULE]
+    - dilarang menggunakan function bawaan javascript kecuali split
+
+    [EXAMPLE]
+
+    wordToArrayReverse("Hello my name is Dimas")
+    output:["Dimas","is","name","my","Hello"]
+
+    wordToArrayReverse("Lorem ipsum sit dolor amet")
+    output:["amet","dolor","sit","ipsum","Lorem"]
+
+*/
+
+function wordToArrayReverse(str) {
+    // your code here 
+}
+
+wordToArrayReverse("Hello my name is Dimas")
+//["Dimas","is","name","my","Hello"]
+
+wordToArrayReverse("Lorem ipsum sit dolor amet")
+//["amet","dolor","sit","ipsum","Lorem"]
+
---
+++ Array/002.js
@@ -0,0 +1,46 @@
+/*
+    ======================
+    String to Multidimensi
+    ======================
+
+    [INSTRUKSI]
+    diberikan sebuah string yang jumlah characternya bisa di akar pangkat
+    (4, 9, 16, 25 , ... ) 
+    ubahlah karakter - karakter itu menjadi array multidimensi
+    dimana hasil akar pangkat jumlah karakternya menjadi row dan col
+
+    [EXAMPLE]
+
+    stringToMultidimensi("0120194124213712")
+    jumlah char : 16 
+    row dan col = akat 16 
+    row dan col = 4
+    output: 
+    [
+         1 2 3 4 
+        [0,1,2,0], 1 
+        [1,9,4,1], 2 
+        [2,4,2,1], 3
+        [3,7,1,2]  4
+    ]
+
+ */
+
+ function stringToMultidimensi(str) {
+     // your code here 
+ }
+
+ stringToMultidimensi("0120194124213712")
+ /*
+    [
+        [0,1,2,0],
+        [1,9,4,1],
+        [2,4,2,1],
+        [3,7,1,2]
+    ]
+ */
+
+stringToMultidimensi("01201941242137127")
+ /*
+    // char count invalid
+ */
