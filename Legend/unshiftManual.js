1fae762fdef6b4f36ead1d9e86e6fcb65b3a6a49
kunglaw <alhusna901@gmail.com>
2019-03-12 16:18:40 +0700

    add unshitManual.js to legend

---
+++ Legend/unshiftManual.js
@@ -0,0 +1,22 @@
+/* 
+    =============
+    unshiftManual
+    =============
+
+    [INSTRUCTION]
+    buatlah sebuah function yang berfungsi untuk 
+    menambahkan data di depan array, sama seperti
+    kegunaan function unshift 
+
+    [RULE]
+    - tidak boleh menambah variable baru atau array baru
+    didalam program 
+
+*/
+
+function unshiftManual(data,adds) {
+    //
+}
+
+console.log( unshiftManual([1,2,3,4],12) );
+// [12,1,2,3,4]
