a64b7a2dd2c64d41bd456712729a832dd70d3e37
kunglaw <alhusna901@gmail.com>
2019-03-14 15:25:29 +0700

    add alphabetMultidimension

---
+++ Array/005.js
@@ -0,0 +1,40 @@
+/*
+    =======================
+    Alphabet Multidimension
+    =======================
+
+    Buatlah sebuah function yang berfungsi mengenerate Alphabet Multidimension. 
+    yang artinya di dalam array multidimension terdapat alphabet yang isinya 
+    berurutan. dimana Array multidimension nya memiliki row dan col yang sama
+    
+    [EXAMPLE]
+    alphabetMultidimension(6)
+
+    maka output nya adalah : 
+
+    [
+        [A,B,C,D,E,F],
+        [G,H,I,J,K,L],
+        [M,N,O,P,Q,R],
+        [S,T,U,V,W,X],
+        [Y,Z,A,B,C,D], <--- perhatikan, jika sudah sampai z maka , kembali lagi ke A
+        [E,F,G,H,I,J]
+    ]
+*/
+
+function alphabetMultidimension(num){
+    // code
+}
+
+console.log( alphabetMultidimension(6) )
+/*
+output:
+[
+    [A,B,C,D,E,F],
+    [G,H,I,J,K,L],
+    [M,N,O,P,Q,R],
+    [S,T,U,V,W,X],
+    [Y,Z,A,B,C,D],
+    [E,F,G,H,I,J]
+]
+*/
